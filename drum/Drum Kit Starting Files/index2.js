9cc9bcb8a97800337ff83bf29b5a18f17360763b
Anita-upadhyay <154465297+Anita-upadhyay@users.noreply.github.com>
2025-08-25 18:28:07 +0530

    Add files via upload

---
+++ drum/Drum Kit Starting Files/index2.js
@@ -0,0 +1,56 @@
+var len= document.querySelectorAll(".drum").length;
+
+ for(var i=0;i<len;i++)
+ {
+    document.querySelectorAll(".drum")[i].addEventListener( "click" ,function()
+     { var buttonInnerhtml= this.innerHTML;
+        makesound(buttonInnerhtml); 
+        buttonInnerhtml
+        
+      
+    } );  
+    
+    } 
+   document.addEventListener("keypress",function(event)
+  {
+      makesound(event.key);
+  });
+   function makesound(key)
+   {  var audio;
+    switch (key) {
+        case "w":
+             audio=new Audio("sounds/tom-1.mp3") ;
+            audio.play();
+            break;
+        case "a":
+             audio=new Audio("sounds/tom-2.mp3") ;
+              audio.play();
+               break;
+      case "s":
+             audio=new Audio("sounds/tom-3.mp3") ;
+            audio.play();
+            break;
+     case "d":
+             audio=new Audio("sounds/tom-4.mp3") ;
+             audio.play();
+            break; 
+  case "j":
+            audio=new Audio("sounds/kick-bass.mp3") ;
+              audio.play();
+            break; 
+   case "k":
+             audio=new Audio("sounds/snare.mp3") ;
+             audio.play();
+            break;    
+ case "l":
+             audio=new Audio("sounds/crash.mp3") ;
+              audio.play();
+            break;
+
+        default: 
+          console.log(buttonInnerhtml);
+      
+       }   
+      
+    
+   }
---
+++ drum/Drum Kit Starting Files/wrote.js
@@ -0,0 +1,9 @@
+function Bellboy(name, age, permitofwork, language) {
+    this.name = name;
+    this.age = age;
+    this.permitofwork = permitofwork;
+    this.language = language;
+}
+
+var Bellboy1 = new Bellboy("vivan", 10, true, ["english", "french"]);
+console.log(Bellboy1);
